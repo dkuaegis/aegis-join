808227c6b12f8bdc3acf8c9ea581aca0ec751580
amemome <152237136+Amemome@users.noreply.github.com>
2025-01-17 14:47:56 +0900

    Feat/create discord page (#15)
    * feat: 에브리타임 페이지 수정. 아이콘 추가, 제출버튼 이동
    
    * feat: 디스코드 컴포넌트 생성
    
    feat: Discord.tsx에서 디스코드 링크 가져오는 함수랑, 검사하는 폴링 함수 추가. 테스트를 위해 환경변수로 주소를 넣음.
    
    feat: Survey.tsx 에서 아이콘 추가
    
    * feat: 버튼을 화면에 고정된 자리에 위치하게 함.
    
    * fix: check 돌리기
    
    * fix: 쓰지 않은 상태 처리

--- src/components/Discord.tsx
+++ src/components/Discord.tsx
@@ -1,7 +1,109 @@
+import {
+  CheckCircleIcon,
+  CircleHelp,
+  ExternalLink,
+  LoaderCircle,
+} from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { Button } from "./ui/button";
+
 function Discord() {
+  const [discordLink, setDiscordLink] = useState<string>("");
+  const [isChecking, setIsChecking] = useState<boolean>(false);
+  const [isDiscordInvitedSuccess, setIsDiscordInvitedSuccess] =
+    useState<boolean>(false);
+
+
+  useEffect(() => {
+    const fetchDiscordLink = async () => {
+      try {
+        const response = await fetch(import.meta.env.VITE_GET_DISCORD_URL);
+        if (!response.ok) {
+          throw new Error(`HTTP ERROR! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        setDiscordLink(data.discordLink);
+      } catch (err: unknown) {
+      } finally {
+        setIsChecking(true);
+      }
+    };
+
+    fetchDiscordLink();
+  }, []);
+
+  const startPolling = useCallback(() => {
+    let attempts = 0;
+    const interval = 2000;
+
+    const poll = async () => {
+      try {
+        const response = await fetch(import.meta.env.VITE_GET_DISCORD_CHECK);
+        if (!response.ok) {
+          throw new Error("HTTP ERROR");
+        }
+        const data = await response.json();
+
+        if (data.status === "COMPLETE") {
+          setIsDiscordInvitedSuccess(true);
+          setIsChecking(false);
+          return;
+        }
+        attempts++;
+        setTimeout(poll, interval);
+      } catch (err: unknown) {
+        setIsChecking(false);
+      }
+    };
+
+    setIsChecking(true);
+    poll();
+  }, []);
+
+  function discordCheck() {
+    window.open(discordLink, "_blank");
+    startPolling();
+  }
+
   return (
     <div className="space-y-4">
       <h3 className="font-semibold text-lg">디스코드 연동</h3>
+      <Alert>
+        <CircleHelp className="h-4 w-4" />
+        <AlertTitle>디스코드 연동이 왜 필요한가요?</AlertTitle>
+        <AlertDescription>어쩌구 저쩌구</AlertDescription>
+      </Alert>
+      <div className="flex items-center justify-between rounded-lg bg-secondary p-4">
+        <span className="truncate font-medium text-sm shadow-none">
+          {discordLink}
+        </span>
+        <Button
+          className="shadow-none"
+          variant="secondary"
+          onClick={() => discordCheck()}
+        >
+          참여하기
+          <ExternalLink className="mr-2 h-4 w-2" />
+        </Button>
+      </div>
+      <div className="flex items-center justify-center">
+        {isDiscordInvitedSuccess && isChecking ? (
+          <>
+            <CheckCircleIcon className="h-8 w-8 text-green-400" />
+            <p className="pl-4 text-green-400">가입 확인이 완료되었습니다 !</p>
+          </>
+        ) : (
+          <>
+            <LoaderCircle
+              className="h-8 w-8 animate-spin text-gray-500"
+              style={{ animation: "spin 3s linear infinite" }}
+            />
+            <p className="pl-4">디스코드 가입 확인 중입니다 . . .</p>
+          </>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Everytime.tsx
+++ src/components/Everytime.tsx
@@ -2,28 +2,41 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ClockAlert, Link } from "lucide-react";
 
 function Everytime() {
   return (
-    <div className="space-y-4">
+    <div className="mb-12 space-y-4">
       <h3 className="font-semibold text-lg">에브리타임 시간표 제출</h3>
       <Alert>
-        <AlertTitle>시간표 제출 안내</AlertTitle>
+        <ClockAlert className="h-4 w-4" />
+
+        <AlertTitle>시간표 제출이 왜 필요한가요?</AlertTitle>
         <AlertDescription>
           활동을 계획할 때 수업과 겹치지 않게 계획하기 위해서 시간표가 필요해요.
-          시간표 정보는 익명으로 저장돼요.
         </AlertDescription>
       </Alert>
-      <div className="space-y-2">
+      <div className="my-10 space-y-2">
         <Label htmlFor="timetableLink">에브리타임 시간표 링크</Label>
-        <Input
-          id="timetableLink"
-          name="timetableLink"
-          placeholder="https://everytime.kr/timetable/..."
-          required
-        />
+        <div className="items-right flex space-x-4">
+          <div className="relative flex-1">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <Link className="h-4 w-4 text-gray-500" />
+            </div>
+            <Input
+              id="timetableLink"
+              type="timetableLink"
+              name="timetableLink"
+              placeholder="https://everytime.kr/timetable/..."
+              className="pl-10"
+              required
+            />
+          </div>
+          <Button className="inline" type="submit">
+            제출
+          </Button>
+        </div>
       </div>
-      <Button type="submit">시간표 제출</Button>
     </div>
   );
 }
