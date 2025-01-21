99eea768e862528a288f22bf422ac2ce3e6b1c46
amemome <meltksq5140@gmail.com>
2025-01-21 21:01:49 +0900

    refactor: Alert 의 반복되는 부분을 AlertBox 컴포넌트로 대체

--- src/components/Everytime.tsx
+++ src/components/Everytime.tsx
@@ -1,9 +1,9 @@
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { CheckCircleIcon, ClockAlert, Link, LoaderCircle } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
+import AlertBox from "./ui/custom/alertbox";
 
 enum LoadingState {
   IDLE = "idle",
@@ -21,19 +21,10 @@ function Everytime({
   const [everytimeLink, setEverytimeLink] = useState<string>("");
   const [loading, setLoading] = useState<LoadingState>(LoadingState.IDLE);
 
-  // 에브리 타임 시간표도 유효성을 검사해야 한다면, validate 로 검사중.... 띄우기.
-
-  useEffect(() => {
-    if (isValid === true) {
-      setLoading(LoadingState.SUCCESS);
-    }
-  }, [isValid]);
-
-  const handleEverytimeValidate = useCallback(async () => {
+  const handleEverytimeSubmit = useCallback(async () => {
+    
     if (loading === LoadingState.LOADING) return;
     if (everytimeLink.trim() === "") return;
-
-    console.log(everytimeLink);
 
     try {
       setLoading(LoadingState.LOADING);
@@ -57,17 +48,50 @@ function Everytime({
     } catch (error) {}
   }, [loading, everytimeLink, onValidate]);
 
+
+  const statusMessage = () => {
+    switch (loading) {
+      case LoadingState.LOADING:
+        return (
+          <>
+            <LoaderCircle
+              className="h-8 w-8 animate-spin text-gray-500"
+              style={{ animation: "spin 3s linear infinite" }}
+            />
+            <p className="pl-4">시간표 정보를 읽는 중 입니다. . .</p>
+          </>
+        );
+      
+      case LoadingState.SUCCESS:
+        return (
+          <>
+            <CheckCircleIcon className="h-8 w-8 text-green-400" />
+            <p className="pl-4 text-green-400">제출이 완료되었습니다 !</p>
+          </>
+        );
+      
+      default:
+        return null;
+    }
+  };
+
+
+  useEffect(() => {
+    if (isValid === true) {
+      setLoading(LoadingState.SUCCESS);
+    } else {
+      setLoading(LoadingState.IDLE);
+    }
+  }, [isValid]);
+
   return (
     <div className="mb-12 space-y-4">
       <h3 className="font-semibold text-lg">에브리타임 시간표 제출</h3>
-      <Alert>
-        <ClockAlert className="h-4 w-4" />
-
-        <AlertTitle>시간표 제출이 왜 필요한가요?</AlertTitle>
-        <AlertDescription>
-          활동을 계획할 때 수업과 겹치지 않게 계획하기 위해서 시간표가 필요해요.
-        </AlertDescription>
-      </Alert>
+      <AlertBox 
+        icon={<ClockAlert className="h-4 w-4" />}
+        title="시간표 제출이 왜 필요한가요?"
+        description="활동을 계획할 때 수업과 겹치지 않게 계획하기 위해서 시간표가 필요해요."
+      />
       <div className="my-10 space-y-2">
         <Label htmlFor="timetableLink">에브리타임 시간표 링크</Label>
         <div className="items-right flex space-x-4">
@@ -88,26 +112,13 @@ function Everytime({
           <Button
             className="inline"
             type="submit"
-            onClick={handleEverytimeValidate}
+            onClick={handleEverytimeSubmit}
           >
             제출
           </Button>
         </div>
         <div className="flex items-center justify-center pt-4">
-          {loading === LoadingState.IDLE ? null : loading === LoadingState.LOADING ? (
-            <>
-              <LoaderCircle
-                className="h-8 w-8 animate-spin text-gray-500"
-                style={{ animation: "spin 3s linear infinite" }}
-              />
-              <p className="pl-4">시간표 정보를 읽는 중 입니다. . .</p>
-            </>
-          ) : (
-            <>
-              <CheckCircleIcon className="h-8 w-8 text-green-400" />
-              <p className="pl-4 text-green-400">제출이 완료되었습니다 !</p>
-            </>
-          )}
+          {statusMessage()}
         </div>
       </div>
     </div>
