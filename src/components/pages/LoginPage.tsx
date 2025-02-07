cdfecbec2a275c479d9375295c5cae7143bad0ce
amemome <152237136+Amemome@users.noreply.github.com>
2025-02-07 19:27:09 +0900

    chore: 아이콘 파일 교체 (#32)

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -6,7 +6,7 @@ function LoginPage() {
       <div className="w-full max-w-[400px] space-y-6 p-4">
         <div className="flex flex-col space-y-2 text-center">
           <img
-            src="/aegis-logo.png"
+            src="/aegis-logo.webp"
             alt="Aegis Logo"
             className="mx-auto mb-4 h-32 w-32"
           />
