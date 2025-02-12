caeb6cf11be00e4c0b16e8d90ca7485b6e5d6927
KwonDeaGeun <101159518+KwonDeaGeun@users.noreply.github.com>
2025-02-12 19:47:13 +0900

    Feat: 디스코드 UI 변경 및 데이터 패칭 추가, 에타 중간 저장 추가 (#37)
    * refactor: 에타 페이지 리팩토링
    
    * feat: 전역 상태 관리 추가
    
    * feat: 디스코드 데이터 패칭 및 에타 중간 저장 추가
    
    * chore: 환경변수 변경 및 상속 수정
    
    * chore: 클립보드 복사 함수 컴포넌트로 빼두기기
    
    * chore: 환경 변수 초기화

---
+++ src/components/pages/Discord/Discord.Api.ts
@@ -0,0 +1,43 @@
+export const fetchDiscordCode = async (): Promise<string> => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}api/discord/code`);
+    if (!response.ok) {
+      throw new Error(`HTTP ERROR! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.code;
+  } catch (err: unknown) {
+    console.error("Failed to fetch Discord code:", err);
+    throw err;
+  }
+};
+
+export const startDiscordPolling = async (): Promise<boolean> => {
+  const interval = 2000;
+  let attempts = 0;
+
+  return new Promise((resolve) => {
+    const poll = async () => {
+
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/discord/check`);
+
+        if (!response.ok) {
+          throw new Error(`ERROR on polling: ${response.status}`);
+        }
+
+        if (response.status === 200) {
+          resolve(true);
+          return;
+        }
+      } catch (err) {
+        console.error("디스코드 폴링 중 오류 발생:", err);
+      }
+
+      attempts++;
+      setTimeout(poll, interval);
+    };
+
+    poll();
+  });
+};
