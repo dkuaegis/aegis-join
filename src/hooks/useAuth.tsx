0bc487b3a1efd5b1b37b7dce8bf748db832121b4
Yun Seongmin <ilcm96@gmail.com>
2025-02-16 22:45:00 +0900

    refactor: 인증 정보 체크 API 요청에 credentials 포함하도록 변경

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -7,10 +7,10 @@ export default function useAuth() {
     const checkAuth = async () => {
       try {
         const response = await fetch(
-          `${import.meta.env.VITE_API_URL}/auth/check`
-          //   {
-          //     credentials: "include",
-          //   }
+          `${import.meta.env.VITE_API_URL}/auth/check`,
+            {
+              credentials: "include",
+            }
         );
         if (!response.ok) {
           throw new Error("인증 정보 없음.");
