d3af513c1bc78cbb76309451f29e792be9a62b4c
amemome <meltksq5140@gmail.com>
2025-02-17 14:30:40 +0900

    feat: 토스트 추가한 패치함수 추가

--- src/components/pages/Error/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-import ErrorMessage from "@/components/ui/custom/errorMessage"
-
-export default function NotFound() {
-  return (
-    <ErrorMessage code="404" title="Page Not Found" description="Sorry, we couldn't find the page you're looking for." />
-  )
-}
