193c8cdfb5b792cc539f51ee12db62afc8df803c
KwonDeaGeun <101159518+KwonDeaGeun@users.noreply.github.com>
2025-02-17 19:08:42 +0900

    Refactor: useLayoutEffect 사용 (#44)
    * refactor: 에타 페이지 리팩토링
    
    * feat: 전역 상태 관리 추가
    
    * Merge branch 'main' of https://github.com/dk-aegis/aegis-join
    
    * feat: 쿠폰 페이지 리메이크 병합
    
    * refactor: 다음 버튼 일시적 비활성화 현상 수정
    
    * refactor: useLayoutEffect 사용

--- src/components/ui/custom/navigationButton.tsx
+++ src/components/ui/custom/navigationButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { useLayoutEffect, useState } from "react";
 
 export default function NavigationButtons({
   prev,
@@ -9,13 +10,19 @@ export default function NavigationButtons({
   next: () => void;
   isValid?: boolean;
 }) {
+  const [buttonVariant, setButtonVariant] = useState<"default" | "secondary">("default");
+
+  useLayoutEffect(() => {
+    setButtonVariant(isValid ? "default" : "secondary");
+  }, [isValid]);
+
   return (
     <div className="fixed right-0 bottom-0 left-0 flex justify-center bg-background/80 p-4 backdrop-blur-sm">
       <div className="mx-auto flex w-full max-w-md justify-between px-4 py-4">
         <Button type="button" onClick={prev}>
           이전
         </Button>
-        <Button onClick={next} variant={isValid ? "default" : "secondary"}>
+        <Button onClick={next} variant={buttonVariant}>
           다음
         </Button>
       </div>
