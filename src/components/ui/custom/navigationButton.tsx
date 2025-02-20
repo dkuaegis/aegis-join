9d40ec7cf13129835d6748261de667ca78335355
amemome <meltksq5140@gmail.com>
2025-02-20 20:12:39 +0900

    feat: 버튼을 자연스러운 스타일로 변경

--- src/components/ui/custom/navigationButton.tsx
+++ src/components/ui/custom/navigationButton.tsx
@@ -30,7 +30,7 @@ export default function NavigationButtons({
 
   return (
     <div className="fixed right-0 bottom-0 left-0 flex justify-center bg-background/80 p-4 backdrop-blur-sm">
-      <div className="mx-auto flex w-full max-w-md justify-between px-4 py-4">
+      <div className="mx-auto flex w-full max-w-md justify-between  py-4">
         <Button type="button" onClick={prev} className={navigationButtonStyle(first)}>
           이전
         </Button>
