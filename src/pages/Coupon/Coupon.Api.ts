45dc3660e75b0aae1b9142061c9fe7559ced2311
amemome <meltksq5140@gmail.com>
2025-02-22 22:41:41 +0900

    feat: 쿠폰 코드 적는 버튼추가

--- src/pages/Coupon/Coupon.Api.ts
+++ src/pages/Coupon/Coupon.Api.ts
@@ -30,3 +30,22 @@ export const submitCoupon = async (selectedCoupons: number[]) => {
   }
   return response.json();
 };
+
+export const submitCouponCode = async (couponCode: number) => {
+  const payload = couponCode;
+
+  const response = await fetchingWithToast(
+    `${import.meta.env.VITE_API_URL}/payments`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Error at submit COUPON !");
+  }
+  return response.json();
+};
---
+++ src/pages/Discord/Discord.HowtoDo.tsx
@@ -0,0 +1,15 @@
+
+
+const HowtoDo = () => {
+    return (
+        <div>
+            <p>이러쿵 저러쿵</p>
+            <p>이렇게 저렇게</p>
+        </div>
+    )
+}
+
+
+
+
+export default HowtoDo
