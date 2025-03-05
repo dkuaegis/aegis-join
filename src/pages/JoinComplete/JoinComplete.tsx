3a8607e7b86c30a08256f0177bceb762fae02748
amemome <meltksq5140@gmail.com>
2025-03-05 23:59:51 +0900

    fix: 사용하지 않는 import 주석처리

--- src/pages/JoinComplete/JoinComplete.tsx
+++ src/pages/JoinComplete/JoinComplete.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { MessageCircle } from "lucide-react";
+// import { Button } from "@/components/ui/button";
+// import { MessageCircle } from "lucide-react";
 import Confetti from "react-confetti";
 import CenterLogo from "./JoinComplete.Centerlogo";
 
