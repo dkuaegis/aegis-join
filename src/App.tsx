29771e7906e5e206855ca2f6fc45d5c69f44c0f0
Amemome <meltksq5140@gmail.com>
2025-07-10 21:35:57 +0900

    feat: 경로에서 대문자를 쓰지 않도록 변경

--- src/App.tsx
+++ src/App.tsx
@@ -23,29 +23,29 @@ function App() {
         pauseOnFocusLoss={false}
       />
       <h1 className="font-bold text-2xl">동아리 회원 가입</h1>
-      <Progress value={progress} className="mt-3 mb-3 w-full" />
+      <Progress value={progress} className="mt-3 mb-3 w-full h-0.5" />
       <Routes>
         <Route
-          path="/PersonalInfo"
+          path="/personal-info"
           element={<PersonalInfo onNext={next} onPrev={prev} />}
         />
         <Route
-          path="/Survey"
+          path="/survey"
           element={<Survey onNext={next} onPrev={prev} />}
         />
         <Route
-          path="/Discord"
+          path="/discord"
           element={<Discord onNext={next} onPrev={prev} />}
         />
         <Route
-          path="/Coupon"
+          path="/coupon"
           element={<Coupon onNext={next} onPrev={prev} />}
         />
         <Route
-          path="/Payment"
+          path="/payment"
           element={<Payment onNext={next} onPrev={prev} />}
         />
-        <Route path="/JoinComplete" element={<JoinComplete />} />
+        <Route path="/complete" element={<JoinComplete />} />
 
         <Route path="*" element={<Navigate to={`/${currentStep}`} replace />} />
       </Routes>
