e1ed18952d43b1f49b04c092621cefaed512b99b
Amemome <meltksq5140@gmail.com>
2025-07-11 19:42:19 +0900

    chore: tailwindcss를 vite 플러그인버전으로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
