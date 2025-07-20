841725e904f0355b3bc57812a77ac866fceb2fd5
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-21 01:37:14 +0900

    feat: 에러 메세지 컴포넌트 사용

--- src/pages/Survey/Survey.JoinReason.tsx
+++ src/pages/Survey/Survey.JoinReason.tsx
@@ -2,6 +2,7 @@ import { useFormContext } from "react-hook-form";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import type { SurveyFormValues } from "./Survey.schema";
+import { ErrorMessage } from "@/components/ui/custom/error-message";
 
 const JoinReason = () => {
   const {
@@ -11,23 +12,18 @@ const JoinReason = () => {
 
   return (
     <div className="space-y-2">
-      <Label htmlFor="joinReason" className="flex items-end text-xl">
+      <Label htmlFor="joinReason" className="flex items-end text-base">
         가입 이유{" "}
-        <span
-          className={`pb-1 pl-2 text-red-500 text-xs ${
-            errors.joinReason
-              ? "visibility-visible opacity-100"
-              : "visibility-hidden opacity-0"
-          }`}
-        >
-          *필수 항목입니다(5자 이상)
-        </span>
       </Label>
       <Textarea
         id="joinReason"
-        placeholder="동아리에서 어떤 활동을 하고 싶으신가요?"
+        placeholder="동아리에서 어떤 활동을 하고 싶으신가요? 자유롭게 작성해주세요!"
         maxLength={510}
         {...register("joinReason")}
+      />
+      <ErrorMessage
+        isShown={!!errors.joinReason}
+        message={errors.joinReason?.message}
       />
     </div>
   );
