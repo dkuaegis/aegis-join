d0f8ba0d8848dcf49c577b626350ed8852a68697
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-20 17:16:22 +0900

    feat: button 컴포넌트에 icon variant 추가

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,14 +17,15 @@ const buttonVariants = cva(
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         "outline-form":
           "justify-between border border-input bg-background shadow-xs text-muted-foreground text-input-base hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-          secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+        secondary:
+        "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        "icon": "bg-white text-secondary-foreground hover:bg-primary/10 hover:text-accent-foreground active:bg-primary/10 active:scale-95",
       },
       size: {
-        default: "h-12 px-4 py-2 has-[>svg]:px-3",
+        default: "h-12 px-4 py-2 has-[>svg]:px-4",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-12 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
