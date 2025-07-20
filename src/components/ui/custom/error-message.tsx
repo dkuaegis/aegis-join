cc4550c4e2d86a002ad0a56f5802c756cbf1ac8c
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-20 17:19:06 +0900

    feat: error-message 컴포넌트 추가

---
+++ src/components/ui/custom/error-message.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+interface ErrorMessageProps extends React.HTMLAttributes<HTMLParagraphElement> {
+  isShown?: boolean
+  message?: string
+}
+
+export function ErrorMessage({
+  isShown = false,
+  message,
+  className,
+  ...props
+}: ErrorMessageProps) {
+  return (
+    <p
+      className={cn(
+        "text-xs font-medium",
+        isShown ? "text-red-500" : "invisible", 
+        className
+      )}
+      aria-live="polite"
+      {...props}
+    >
+
+      {message}
+    </p>
+  )
+}
