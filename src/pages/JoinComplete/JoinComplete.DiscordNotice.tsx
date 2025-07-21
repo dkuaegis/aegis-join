a05680eb71adc86531f01ebb35f0ca7f6d3ea1f0
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-21 10:16:55 +0900

    feat: complete 에서 디스코드 버튼 컴포넌트로 변경

--- src/pages/JoinComplete/JoinComplete.DiscordNotice.tsx
+++ src/pages/JoinComplete/JoinComplete.DiscordNotice.tsx
@@ -1,5 +1,6 @@
 import DiscordIcon from "@/assets/discordIcon.svg";
 import { Button } from "@/components/ui/button";
+import DiscordLinkButton from "@/components/ui/custom/discord-link-button";
 
 const DiscordNotice = () => {
   return (
@@ -8,25 +9,10 @@ const DiscordNotice = () => {
         앞으로의 모든 활동과 소통은 <strong>디스코드</strong>에서 이루어집니다.
         Aegis와 함께 성장해 나가요!
       </p>
-      <Button
-        size="lg"
-        className="w-full bg-discord-blue text-white transition-transform duration-100 hover:bg-discord-blue active:scale-95"
-        asChild
-      >
-        <a
-          href={import.meta.env.VITE_DISCORD_NOTICE_URL}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center justify-center gap-1"
-        >
-          <img
-            src={DiscordIcon}
-            alt="Discord Icon"
-            className="h-[24px] w-[24px]"
-          />
-          <span className="text-[16px]">디스코드 공지방 확인하기</span>
-        </a>
-      </Button>
+      <DiscordLinkButton 
+        text="디스코드 공지방"
+        url={import.meta.env.VITE_DISCORD_NOTICE_URL}
+      />
     </div>
   );
 };
