cfc6559ac34072a4d1ab8a411e501b9b88046bb8
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-22 01:31:14 +0900

    feat: join step 에 회칙동의 추가

--- src/constants/joinSteps.ts
+++ src/constants/joinSteps.ts
@@ -1,5 +1,6 @@
 // 각 가입 단계를 나타내는 문자열 배열
 export const JOIN_STEPS: readonly string[] = [
+  "agreement",
   "personal-info",
   "survey",
   "coupon",
@@ -13,6 +14,7 @@ type JoinStep = (typeof JOIN_STEPS)[number];
 
 // 각 단계별 한글 이름을 매핑하는 객체
 export const JOIN_STEP_KOREAN_MAP: { [key in JoinStep]: string } = {
+  agreement: "회칙 동의",
   "personal-info": "기본 인적사항",
   survey: "가입 설문",
   coupon: "쿠폰 사용",
