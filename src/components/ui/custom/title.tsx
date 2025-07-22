88242d1ff3c514a7c697703b144a1a05d9c9ed0e
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-22 09:55:06 +0900

    feat: 이전 버튼 크기 수정

--- src/components/ui/custom/title.tsx
+++ src/components/ui/custom/title.tsx
@@ -5,19 +5,35 @@ import { Button } from "../button";
 
 interface TitleProps {
   currentStep: string;
+  onPrev?: () => void;
 }
 
-const Title = ({ currentStep }: TitleProps) => {
+const Title = ({ currentStep, onPrev }: TitleProps) => {
+
+  const stepKeys = Object.keys(JOIN_STEP_KOREAN_MAP);
+  const isFirstStep = currentStep === stepKeys[0];
+  const isLastStep = currentStep === stepKeys[stepKeys.length - 1];
+
+  if (isFirstStep || isLastStep) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <h1 className="text-2xl font-bold">
+          {JOIN_STEP_KOREAN_MAP[currentStep]}
+        </h1>
+      </div>   
+    )
+  } else {
   return (
     <Stack>
-      <Button variant="icon" size="icon" aria-label="Go back">
-        <ArrowLeftIcon size={16} />
+      <Button  variant="icon" aria-label="Go back" onClick={onPrev}>
+        <ArrowLeftIcon size={32}/>
       </Button>
       <h1 className="font-bold text-2xl">
         {JOIN_STEP_KOREAN_MAP[currentStep]}
       </h1>
     </Stack>
   );
+  }
 };
 
 export default Title;
