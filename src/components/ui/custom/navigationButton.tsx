c3b6590aa682e64e519d158974c8ae4326499e45
Amemome <meltksq5140@gmail.com>
2025-07-22 13:30:44 +0900

    feat: navigation 버튼에서 isValid 만 받아도 됨

--- src/components/ui/custom/navigationButton.tsx
+++ src/components/ui/custom/navigationButton.tsx
@@ -1,25 +1,15 @@
 import { useLayoutEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
+import useFunnel from "@/hooks/useFunnel";
 
 interface NavigationButtonsProps {
-  prev: () => void;
-  next: () => void;
   isValid?: boolean;
-  showPrev?: boolean;
-  showNext?: boolean;
-}
-
-function navigationButtonStyle(visible: boolean) {
-  return `w-[42.5%] ${visible ? "invisible" : ""}`;
 }
 
 export default function NavigationButtons({
-  prev,
-  next,
   isValid,
-  showPrev = false,
-  showNext = false,
 }: NavigationButtonsProps) {
+  const { next } = useFunnel();
   const [buttonVariant, setButtonVariant] = useState<"default" | "secondary">(
     "default"
   );
@@ -32,19 +22,9 @@ export default function NavigationButtons({
     <div className="fixed right-0 bottom-0 left-0 flex justify-center bg-background/80 p-4 backdrop-blur-xs">
       <div className="mx-auto flex w-full max-w-md justify-between py-2">
         <Button
-          type="button"
-          variant="outline"
-          onClick={prev}
-          size="lg"
-          className={navigationButtonStyle(showPrev)}
-        >
-          이전
-        </Button>
-        <Button
           onClick={next}
           variant={buttonVariant}
           size="lg"
-          className={navigationButtonStyle(showNext)}
         >
           다음
         </Button>
