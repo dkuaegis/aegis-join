75b9fc2da9a7ee765bddfc1ba71f5b0571192928
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-22 16:15:53 +0900

    feat: NavigationButton 컴포넌트에 비동기함수 prop 추가

--- src/components/ui/custom/navigationButton.tsx
+++ src/components/ui/custom/navigationButton.tsx
@@ -1,13 +1,17 @@
-import { useLayoutEffect, useState } from "react";
+import { useCallback, useLayoutEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import useFunnel from "@/hooks/useFunnel";
 
 interface NavigationButtonsProps {
   isValid?: boolean;
+  onFetch?: () => Promise<boolean>;
+  text?: string;
 }
 
-export default function NavigationButtons({ isValid }: NavigationButtonsProps) {
+export default function NavigationButtons({ isValid, onFetch, text }: NavigationButtonsProps) {
   const { next } = useFunnel();
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [buttonVariant, setButtonVariant] = useState<"default" | "secondary">(
     "default"
   );
@@ -15,12 +19,41 @@ export default function NavigationButtons({ isValid }: NavigationButtonsProps) {
   useLayoutEffect(() => {
     setButtonVariant(isValid ? "default" : "secondary");
   }, [isValid]);
+  
+  const handleNext = useCallback(async () => {
+    if (isLoading || !isValid) return;
+
+    if(!onFetch) {
+      next();
+      return;
+    }
+
+    setIsLoading(true);
+    try {
+      const isSuccess = await onFetch();
+      if(isSuccess) {
+        next();
+      } else {
+        console.error("Error on fetch");
+      }
+    } catch (error) {
+      console.error("Error on fetch");
+    } finally {
+      setIsLoading(false);
+    }
+  }, [isLoading, isValid, onFetch, next]);
 
   return (
     <div className="fixed right-0 bottom-0 left-0 flex justify-center bg-background/80 p-4 backdrop-blur-xs">
       <div className="mx-auto flex w-full max-w-md justify-between py-2">
-        <Button onClick={next} variant={buttonVariant} size="lg" className="min-w-full">
-          다음
+        <Button
+          onClick={handleNext}
+          variant={buttonVariant}
+          size="lg"
+          className="min-w-full"
+          disabled={isLoading || !isValid}
+        >
+          {text ? text : "다음"}
         </Button>
       </div>
     </div>
