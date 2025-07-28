03acb7f78f29bc9bf5f5680842114a5e09b27e37
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-28 18:25:41 +0900

    style: biome run check

--- src/pages/JoinComplete/JoinComplete.tsx
+++ src/pages/JoinComplete/JoinComplete.tsx
@@ -1,17 +1,16 @@
-// import Confetti from "react-confetti";
-import CenterLogo from "./JoinComplete.Centerlogo";
-import DiscordNotice from "./JoinComplete.DiscordNotice";
-import KakaoChatroom from "./JoinComplete.KakaoChatroom";
 import Lottie from "lottie-react";
 import Rocket from "@/assets/lottie/Rocket.json";
+import DiscordNotice from "./JoinComplete.DiscordNotice";
+import KakaoChatroom from "./JoinComplete.KakaoChatroom";
+
 const JoinComplete = () => {
   return (
     <AlignCenter>
       <Wrapper>
         <Lottie
-        animationData={Rocket}
-        loop={true}
-        style={{ width: 300, height: 300 }}
+          animationData={Rocket}
+          loop={true}
+          style={{ width: 300, height: 300 }}
         />
         <p className="text-muted-foreground text-xl">
           성공적으로 가입이 완료되었습니다.
