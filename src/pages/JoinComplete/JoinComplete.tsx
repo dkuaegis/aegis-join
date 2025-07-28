8a2dc41e13a37ba95811e91010034b0daa3bfccf
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-29 01:12:17 +0900

    refactor: Rocket Icon 에 Lazy loading 적용

--- src/pages/JoinComplete/JoinComplete.tsx
+++ src/pages/JoinComplete/JoinComplete.tsx
@@ -1,17 +1,21 @@
-import Lottie from "lottie-react";
+import { lazy, Suspense } from "react";
 import Rocket from "@/assets/lottie/Rocket.json";
 import DiscordNotice from "./JoinComplete.DiscordNotice";
 import KakaoChatroom from "./JoinComplete.KakaoChatroom";
+
+const Lottie = lazy(() => import("lottie-react"));
 
 const JoinComplete = () => {
   return (
     <AlignCenter>
       <Wrapper>
-        <Lottie
-          animationData={Rocket}
-          loop={true}
-          style={{ width: 300, height: 300 }}
-        />
+        <Suspense fallback={<div style={{ width: 300, height: 300 }} />}>
+          <Lottie
+            animationData={Rocket}
+            loop={true}
+            style={{ width: 300, height: 300 }}
+          />
+        </Suspense>
         <p className="text-muted-foreground text-xl">
           성공적으로 가입이 완료되었습니다.
         </p>
