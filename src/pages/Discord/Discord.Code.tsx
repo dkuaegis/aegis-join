3b5e4cb2b2168f5d28f84bd718464f23c11b2e62
amemome <152237136+Amemome@users.noreply.github.com>
2025-07-29 20:42:13 +0900

    style: biome run check

--- src/pages/Discord/Discord.Code.tsx
+++ src/pages/Discord/Discord.Code.tsx
@@ -1,5 +1,5 @@
+import { CopyIcon, RefreshCcw } from "lucide-react";
 import type React from "react";
-import { RefreshCcw, CopyIcon } from "lucide-react";
 
 interface AuthCodeProps {
   code: string;
@@ -10,32 +10,33 @@ interface AuthCodeProps {
 const DiscordCode: React.FC<AuthCodeProps> = ({ code, onRefresh }) => {
   const handleCopy = () => {
     navigator.clipboard.writeText(code);
-  };  
+  };
   return (
     <div className="flex h-36 flex-col items-center gap-4">
-      <p 
-        className="font-medium text-5xl cursor-pointer p-4 rounded-4xl transition-colors hover:bg-neutral-200"
+      <button
+        className="cursor-pointer rounded-4xl p-4 font-medium text-5xl transition-colors hover:bg-neutral-200"
+        type="button"
         onClick={onRefresh}
       >
         {code}
-      </p>
+      </button>
       <div className="flex gap-4">
-      <button
-        onClick={onRefresh}
-        type="button"
-        className="flex items-center gap-1 text-neutral-500 underline cursor-pointer p-1 rounded-2xl transition-colors hover:bg-neutral-200"
-      >
-        <RefreshCcw size="12" />
-        <span>새로고침</span>
-      </button>
-      <button
-        onClick={handleCopy}
-        type="button"
-        className="flex items-center gap-1 text-slate-500 underline cursor-pointer p-1 rounded-2xl transition-colors hover:bg-neutral-200"
-      >
-        <CopyIcon size={16}/>
-        <span>복사하기</span>
-      </button>
+        <button
+          onClick={onRefresh}
+          type="button"
+          className="flex cursor-pointer items-center gap-1 rounded-2xl p-1 text-neutral-500 underline transition-colors hover:bg-neutral-200"
+        >
+          <RefreshCcw size="12" />
+          <span>새로고침</span>
+        </button>
+        <button
+          onClick={handleCopy}
+          type="button"
+          className="flex cursor-pointer items-center gap-1 rounded-2xl p-1 text-slate-500 underline transition-colors hover:bg-neutral-200"
+        >
+          <CopyIcon size={16} />
+          <span>복사하기</span>
+        </button>
       </div>
     </div>
   );
