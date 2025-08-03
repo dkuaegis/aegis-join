d70172fa0b0c2103337cd1d71ca34e43eddeacf9
amemome <152237136+Amemome@users.noreply.github.com>
2025-08-03 21:33:44 +0900

    feat: 설문조사 변경된 api 적용

--- src/pages/PersonalInfo/PersonalInfo.tsx
+++ src/pages/PersonalInfo/PersonalInfo.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import NavigationButtons from "@/components/ui/custom/navigationButton";
 import useFunnel from "@/hooks/useFunnel";
@@ -14,69 +14,84 @@ import {
   submitPersonalInfoData,
 } from "./PersonalInfo.Api";
 import {
+  PersonalInfoApiValues,
   type PersonalInfoFormValues,
   personalInfoSchema,
 } from "./PersonalInfo.schema";
+import { useNextStep } from "@/hooks/useNextStep";
+import { transformFetchedDataToFormValues } from "./PersonalInfo.helper";
+
+
 
 const PersonalInfo = () => {
-  const { next } = useFunnel();
-  const { personalInfoData, setPersonalInfoData, isInitial, setNotInitial } =
-    usePersonalInfoStore();
+  const { personalInfoData, setPersonalInfoData } = usePersonalInfoStore();
+  const isInitial = personalInfoData === null;
+  const [isFetching, setIsFetching] = useState(isInitial);
+
+  const handleSubmitWithTransform = async (formData: PersonalInfoFormValues) => {
+      // 주민등록번호로 성별 계산
+      const gender = Number(formData.residentNumber_back) % 2 ? "MALE" : "FEMALE";
+      
+      // API에 보낼 데이터 가공
+      const { residentNumber_back, ...rest } = formData;
+      const apiData: PersonalInfoApiValues = { ...rest, gender };
+
+      // 실제 API 호출 함수 실행
+      return submitPersonalInfoData(apiData);
+  };
+
+  const { isLoading, handleSubmit } = useNextStep(handleSubmitWithTransform);
+
+  const defaultValues = personalInfoData || {};
 
   const methods = useForm<PersonalInfoFormValues>({
     resolver: zodResolver(personalInfoSchema),
     mode: "onChange",
-    defaultValues: personalInfoData || {},
+    defaultValues,
   });
 
   useEffect(() => {
     if (isInitial) {
       fetchPersonalInfoData()
-        .then((data: PersonalInfoFormValues) => {
-          setPersonalInfoData(data);
-          methods.reset(data);
-          setNotInitial();
+        .then((data) => {
+          const transformedData = transformFetchedDataToFormValues(data);
+          setPersonalInfoData(transformedData);
+          methods.reset(transformedData);
         })
-        .catch(console.error);
+        .catch(console.error)
+        .finally(() => setIsFetching(false));
     }
 
     return () => {
-      setPersonalInfoData(methods.getValues());
+      if (methods.formState.isDirty) {
+        setPersonalInfoData(methods.getValues());
+      }
     };
   }, [
-    isInitial,
     methods.reset,
     methods.getValues,
-    setNotInitial,
     setPersonalInfoData,
   ]);
-
-  const onSubmit = (data: PersonalInfoFormValues) => {
-    submitPersonalInfoData(data)
-      .then(() => {
-        setPersonalInfoData(data);
-        next();
-      })
-      .catch((error) => {
-        console.error("제출 중 오류가 발생했습니다:", error);
-      });
-  };
 
   return (
     <FormProvider {...methods}>
       <form
         className="line-breaks space-y-4"
-        onSubmit={methods.handleSubmit(onSubmit)}
+        onSubmit={methods.handleSubmit(handleSubmit)}
       >
         <StudentPhoneNumber name="phoneNumber" />
         <StudentId name="studentId" />
         <StudentDepartment name="department" />
         <StudentGrade name="grade" />
         <StudentResidentNumber />
-        <NavigationButtons isValid={methods.formState.isValid} />
+        <NavigationButtons 
+          disabled={!methods.formState.isValid} 
+          isLoading={isLoading} 
+        />
       </form>
     </FormProvider>
   );
 };
 
 export default PersonalInfo;
+
