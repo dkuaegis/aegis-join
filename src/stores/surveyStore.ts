5e25135d4242120df7e4ce13d0ab6c228141a632
Amemome <meltksq5140@gmail.com>
2025-08-05 21:19:17 +0900

    feat: survey store 쓰지 않는 필드 제거

--- src/stores/surveyStore.ts
+++ src/stores/surveyStore.ts
@@ -1,32 +1,22 @@
 import { create } from "zustand";
-import type { SurveyFormValues } from "@/pages/Survey/Survey.schema";
-import { AcquisitionType, type InterestField } from "@/types/api/survey";
+import { AcquisitionType } from "@/types/api/survey";
+import { SurveyFormValues, surveySchema } from "@/pages/Survey/Survey.schema";
 
 interface SurveyState {
-  interests: InterestField[];
-  interestsEtc: Partial<Record<InterestField, string | undefined>>;
   joinReason: string;
-  feedback: string;
-  acquisitionType: AcquisitionType | undefined;
+  acquisitionType: string | undefined;
 
   setFormValues: (values: Partial<SurveyFormValues>) => void;
 }
 
 export const useSurveyStore = create<SurveyState>((set) => ({
-  interests: [],
-  interestsEtc: {},
   joinReason: "",
-  feedback: "",
   acquisitionType: undefined,
 
-  setFormValues: (values) =>
-    set((state) => ({
+  setFormValues: (values) => {
+        set((state) => ({
       ...state,
       ...values,
-      acquisitionType: Object.values(AcquisitionType).includes(
-        values.acquisitionType as AcquisitionType
-      )
-        ? (values.acquisitionType as AcquisitionType)
-        : undefined,
-    })),
+    }));
+  },
 }));
