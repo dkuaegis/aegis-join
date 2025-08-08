9c97f091fa12f465bdd0dbcdf86557ce0dd35fcb
BCD1me <152237136+Amemome@users.noreply.github.com>
2025-08-08 20:15:31 +0900

    feat(JOIN-156): 완료 페이지 레이아웃 수정 (#131)
    * feat: 완료 페이지 내용 수정
    
    * feat: 카카오톡 아이콘 svg 에서 배경 사각형 제거
    
    * feat: 카카오톡 참여를 모달/드로워로 변경
    
    * style: biome run check

--- src/pages/JoinComplete/JoinComplete.DiscordNotice.tsx
+++ src/pages/JoinComplete/JoinComplete.DiscordNotice.tsx
@@ -3,9 +3,9 @@ import DiscordLinkButton from "@/components/ui/custom/discord-link-button";
 const DiscordNotice = () => {
   return (
     <div className="break-words pt-8">
-      <p className="line-breaks mb-1 text-center text-lg">
-        앞으로의 모든 활동과 소통은 <strong>디스코드</strong>에서 이루어집니다.
-        Aegis와 함께 성장해 나가요!
+      <p className="line-breaks mb-1 text-center text-muted-foreground">
+        주요 활동과 소통은 <strong>디스코드</strong>에서 이루어져요. Aegis와
+        함께 성장해 나가요!
       </p>
       <DiscordLinkButton
         text="디스코드 공지방"
--- src/pages/JoinComplete/JoinComplete.tsx
+++ src/pages/JoinComplete/JoinComplete.tsx
@@ -7,32 +7,24 @@ const Lottie = lazy(() => import("lottie-react"));
 
 const JoinComplete = () => {
   return (
-    <AlignCenter>
-      <Wrapper>
-        <Suspense fallback={<div style={{ width: 300, height: 300 }} />}>
-          <Lottie
-            animationData={Rocket}
-            loop={true}
-            style={{ width: 300, height: 300 }}
-          />
-        </Suspense>
-        <p className="text-muted-foreground text-xl">
-          성공적으로 가입이 완료되었습니다.
-        </p>
-        <DiscordNotice />
-        <KakaoChatroom />
-      </Wrapper>
-    </AlignCenter>
+    <Wrapper>
+      <Suspense fallback={<div style={{ width: 300, height: 300 }} />}>
+        <Lottie
+          animationData={Rocket}
+          loop={true}
+          style={{ width: 300, height: 300 }}
+        />
+      </Suspense>
+      <p className="mt-4 mb-16 font-bold text-3xl">등록이 완료됐어요</p>
+      <DiscordNotice />
+      <KakaoChatroom />
+    </Wrapper>
   );
 };
 
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div className="space-y-4">{children}</div>;
-};
-
-const AlignCenter = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-1 items-center justify-center p-6 text-center">
+    <div className="mx-auto mt-16 mb-8 w-full max-w-md space-y-4 px-4 py-8 pb-28 text-center">
       {children}
     </div>
   );
