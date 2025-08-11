fb6db14628d06fe48ed86fbebf74809f2bd4f99c
Amemome <meltksq5140@gmail.com>
2025-08-11 13:55:05 +0900

    feat: main에 토스터 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 import App from "./App.tsx";
 import Authentication from "./components/auth/authentication.tsx";
+import { Toaster } from "react-hot-toast";
 
 // biome-ignore lint/style/noNonNullAssertion: The 'root' element is guaranteed to exist in the HTML.
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Authentication>
+        <Toaster 
+          position="bottom-center"
+        />
         <App />
       </Authentication>
     </BrowserRouter>
