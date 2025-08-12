1a9f1916368b1806f5de8f6f652f54c820d997b7
BCD1me <152237136+Amemome@users.noreply.github.com>
2025-08-12 11:30:55 +0900

    chore: 백엔드 변경에 따른 수정 (#143)

--- src/pages/Coupon/Coupon.Api.ts
+++ src/pages/Coupon/Coupon.Api.ts
@@ -2,7 +2,7 @@ import { httpClient } from "@/api/api";
 import type { Coupon } from "./Coupon.Types";
 
 export const fetchCoupon = async (): Promise<Coupon[]> => {
-  return httpClient.get<Coupon[]>("/coupons/issued/me");
+  return httpClient.get<Coupon[]>("/coupons/me");
 };
 
 export const submitCoupon = async (selectedCoupons: number[]) => {
